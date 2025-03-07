8cf13915632620d9d6e492d2192d2fbc651f4dd0
sec-ml <150138296+sec-ml@users.noreply.github.com>
2025-03-07 15:19:22 +0000

    Add `@astrojs/sitemap` #2
    Add sitemap plugin
    `site` must be referenced in `astro.config.mjs` - import `URL` from `config.js`

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
+
+import { CONFIG } from "./src/config/config.js";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
       plugins: [tailwindcss()],
     },
+    site: CONFIG.URL,
+    integrations: [sitemap()],
   });
