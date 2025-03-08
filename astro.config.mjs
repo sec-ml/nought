738ce467fcb63c8c1b38967b744553af0a46c51c
sec-ml <150138296+sec-ml@users.noreply.github.com>
2025-03-08 21:19:02 +0000

    Add astro-robots-txt (#2)

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import sitemap from "@astrojs/sitemap";
 
 import { CONFIG } from "./src/config/config.js";
 
+import robotsTxt from "astro-robots-txt";
+
 // https://astro.build/config
 export default defineConfig({
     vite: {
       plugins: [tailwindcss()],
     },
     site: CONFIG.URL,
-    integrations: [sitemap()],
+    integrations: [sitemap(), robotsTxt()],
   });
