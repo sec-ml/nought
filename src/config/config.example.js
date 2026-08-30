a09bb56e79be60ead58e406aaee57f33b4e071b6
sec-ml <150138296+sec-ml@users.noreply.github.com>
2026-08-30 17:03:24 +0100

    Update config.example.js

--- src/config/config.example.js
+++ src/config/config.example.js
@@ -14,7 +14,7 @@ export const CONFIG = {
 
   TITLE: "Nought",
   LOGO_TEXT: "nought.", // IF DIFFERENT FROM TITLE
-  DESCRIPTION: "An Astro 5 starter with Tailwind CSS",
+  DESCRIPTION: "An Astro 7 starter with Tailwind CSS",
   DEFAULT_AUTHOR: "ml",
   POSTS_DIR: "src/dummy_posts", // RELATIVE TO PROJECT DIRECTORY, OR ABSOLUTE PATH
   TAG_PAGES_DIR: "src/dummy_tag-pages", // RELATIVE TO PROJECT DIRECTORY, OR ABSOLUTE PATH. For custom content on tag page, i.e. /tags/<tag-name>
